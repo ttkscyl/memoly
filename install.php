2ca02ba3ace2ee86870942fc19d11b068030c632
Yilang Chen <ttkscyl@gmail.com>
2025-10-01 18:16:22 +0100

    database creation minor error correction

--- install.php
+++ install.php
@@ -5,12 +5,12 @@ include_once("connection.php");
 $conn->exec("DROP TABLE IF EXISTS Users");
 $conn->exec("
     CREATE TABLE Users (
-        user_id INT(6) AUTO_INCREMENT PRIMARY KEY,
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(30),
         username VARCHAR(30) UNIQUE,
         email VARCHAR(30) UNIQUE,
         password VARCHAR(200) NOT NULL,
-        is_teacher BOOLEAN NOT NULL
+        is_teacher BOOLEAN NOT NULL DEFAULT 0
     )
 ");
 echo "<br>1. Users table created";
@@ -19,11 +19,11 @@ echo "<br>1. Users table created";
 $conn->exec("DROP TABLE IF EXISTS Decks");
 $conn->exec("
     CREATE TABLE Decks (
-        deck_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+        deck_id INT AUTO_INCREMENT PRIMARY KEY,
         user_id INT(6),
         title VARCHAR(30) NOT NULL,
         description VARCHAR(100),
-        created_at VARCHAR(6) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         topic_id INT(3)
     )
 ");
@@ -33,7 +33,7 @@ echo "<br>2. Decks table created";
 $conn->exec("DROP TABLE IF EXISTS Cards");
 $conn->exec("
     CREATE TABLE Cards (
-        card_id INT(20) AUTO_INCREMENT PRIMARY KEY,
+        card_id INT AUTO_INCREMENT PRIMARY KEY,
         deck_id INT(4),
         front VARCHAR(100),
         back VARCHAR(1000) NOT NULL,
@@ -47,7 +47,7 @@ echo "<br>3. Cards table created";
 $conn->exec("DROP TABLE IF EXISTS StudySession");
 $conn->exec("
     CREATE TABLE StudySession (
-        session_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+        session_id INT AUTO_INCREMENT PRIMARY KEY,
         user_id INT(6),
         deck_id INT(4),
         starttime DATETIME,
@@ -62,7 +62,7 @@ echo "<br>4. StudySession table created";
 $conn->exec("DROP TABLE IF EXISTS Topic");
 $conn->exec("
     CREATE TABLE Topic (
-        topic_id INT(3) AUTO_INCREMENT PRIMARY KEY,
+        topic_id INT AUTO_INCREMENT PRIMARY KEY,
         topic_name VARCHAR(200)
     )
 ");
@@ -72,10 +72,10 @@ echo "<br>5. Topic table created";
 $conn->exec("DROP TABLE IF EXISTS Folders");
 $conn->exec("
     CREATE TABLE Folders (
-        folder_id INT(4) AUTO_INCREMENT PRIMARY KEY,
+        folder_id INT AUTO_INCREMENT PRIMARY KEY,
         folder_name VARCHAR(30) NOT NULL,
         folder_description VARCHAR(100),
-        deck_id INT(4) UNIQUE
+        deck_id INT(4)
     )
 ");
 echo "<br>6. Folders table created";
