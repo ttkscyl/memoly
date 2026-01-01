c59b0f20984fe91ca2ca00f3e1ced34660eff72a
Yilang Chen <ttkscyl@gmail.com>
2026-01-01 18:04:25 +0800

    新版本 - connection.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['CurrentUser'])) // checks if user is currently logged in 
-{
-    unset($_SESSION['CurrentUser']); // reset session variable
-}
-header('location: login.php') // redirects to login page
-?>
